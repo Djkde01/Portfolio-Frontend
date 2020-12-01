10cb3839ecfabe006177ae9fe53cf9b8445bc02f
Djkde <sergio.estrella@utp.edu.co>
2020-12-01 02:39:51 -0500

    Added responsive header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import '../styles/Header.css'
 import logo from '../assets/media/LogoVectorWhite.svg';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
+    const [click, setClick] = useState(false);
+
+    const handleClick = () =>{
+        setClick(!click);
+    }
+
+    const closeMobileMenu = () =>{
+        setClick(false);
+    }
+
     return (
         <header className="header">
-            <span className="header-logo">
-                <a href="/"><img src={logo} alt="Djkde Logo"/></a>
-            </span>
-            <nav className="header-menu">
-                <ul className="header-menu--list">
-                    <li className="header-menu--item"><a href="/about">About</a></li>
-                    <li className="header-menu--item"><a href="/">Content</a></li>
-                    <li className="header-menu--item"><a href="/">Projects</a></li>
-                    <li className="header-menu--item"><a href="/">Contact</a></li>
+            <Link to="/" className="header-logo">
+                <img src={logo} alt="Djkde Logo"/>
+            </Link>            
+            <nav>
+                <ul className={click ? "header-menu--list active" : "header-menu--list"}>
+                    <li className="header-menu--item">
+                        <Link to="/about" onClick={closeMobileMenu}>About</Link>
+                    </li>
+                    <li className="header-menu--item">
+                        <Link to="/">Content</Link>
+                    </li>
+                    <li className="header-menu--item">
+                        <Link to="/projects">Projects</Link>
+                    </li>
+                    <li className="header-menu--item">
+                        <Link to="/">Contact</Link>
+                    </li>
                 </ul>
+            <div className={click ? "pencet Diam" : "pencet"} onClick={handleClick}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
             </nav>
         </header>
     )
