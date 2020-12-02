09ac431425a28787b4be9e22955e330ddd8464e2
Djkde <sergio.estrella@utp.edu.co>
2020-12-01 22:50:58 -0500

    Transition effect done

--- src/App.js
+++ src/App.js
@@ -9,18 +9,26 @@ import Projects from './pages/Projects';
 export default withRouter (function App ({ location }) { 
   const [currentPath, setCurrentPath] = useState(location.pathname);
 
+  const [ position, setPosition ] = useState("down");
+
   useEffect(() => {
     const { pathname } = location;
+    if (pathname !== currentPath){
+      setPosition("up")
+    }
     console.log("New path:", pathname, "Previous path: ", currentPath);
     setCurrentPath(pathname);
+    setTimeout(function(){setPosition("down")},2000);
   }, [location.pathname]);
 
   return (
+    <>
+      <Transition position={position}/>
       <Switch>
         <Route exact path="/" component={Landing}/>
         <Route exact path="/about" component={About}/>
         <Route exact path="/projects" component={Projects}/>
-        <Route exact path="/Transition" component={Transition}/>
       </Switch>
+    </>
   );
 });
--- src/components/Transition.jsx
+++ src/components/Transition.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 import Logo from '../assets/media/LogoVectorWhite.svg';
 import '../styles/Transition.css';
 
-const Transition = () => {
+const Transition = ({position}) => {
     return (
-        <div className="transition-page">
+        <div className={`transition-page ${position}`}>
             <span className="transition-page--icon">
                 <img src={Logo} alt="Transition logo"/>
             </span>
