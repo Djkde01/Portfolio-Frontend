151cc9ef58bc5afe8d82ceacafc293702e792454
Djkde <sergio.estrella@utp.edu.co>
2020-12-10 16:11:29 -0500

    Added data.js

---
+++ src/data.js
@@ -0,0 +1,75 @@
+const data = {
+    roles:[
+        {
+            _id:'1',
+            name:'Design',
+            description:'I design beatiful and simple things. Focused on the user and the message of your brand or product.<br/>Things I enjoy designing: UX, UI, Web, Mobile, Logos.',
+            icon:'/assets/icons/design.svg'
+        },
+        {
+            _id:'2',
+            name:'Development',
+            description:'I can help you to bring your product idea into reality.<br/>Offering to you services of development, integration, deployment, analytics, and maintenance.',
+            icon:'/assets/icons/frontend.svg'
+        },
+    ],
+    skills :[
+        {
+            _id:'1',
+            name: 'HTML',
+            image: '/assets/icons/html.svg',
+            
+        },
+        {
+            _id:'2',
+            name: 'CSS',
+            image: '/assets/icons/',
+        },
+        {
+            _id:'3',
+            name: 'React.js',
+            image: '/assets/icons/react.svg',
+        },
+        {
+            _id:'4',
+            name: 'JavaScript',
+            image: '/assets/icons/javascript.svg',
+        },
+        {
+            _id:'5',
+            name: 'SASS',
+            image: '/assets/icons/.svg',
+        },
+        {
+            _id:'6',
+            name: 'Figma',
+            image: '/assets/icons/.svg',
+        },
+        {
+            _id:'7',
+            name: 'Photoshop',
+            image: '/assets/icons/photoshop.svg',
+            
+        },
+        {
+            _id:'8',
+            name: 'Notion',
+            image: '/assets/icons/notion.svg',
+            
+        },
+        {
+            _id:'9',
+            name: 'Git',
+            image: '/assets/icons/git.svg',
+            
+        },
+        {
+            _id:'10',
+            name: 'GitHub',
+            image: '/assets/icons/github.svg',
+            
+        },
+    ],
+};
+
+export default data;
